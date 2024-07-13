01b1ebdc8383e8ba95b733366f7ab1d1991dbb74
Edy Segura <edysegura@gmail.com>
2024-07-13 16:29:55 -0300

    feat: add html table

--- localStorage/app.js
+++ localStorage/app.js
@@ -26,14 +26,21 @@ class App {
     const ls = window.localStorage;
     if (!ls.length) return;
     const lsKeys = Object.keys(ls);
-    const allValues = lsKeys.map(this.toHTML);
-    console.log(allValues);
+    const allValues = lsKeys.map(this.toHTML).join('');
+    // this.addToHTML(allValues);
   }
 
   toHTML(key) {
     const value = window.localStorage.getItem(key);
     const html = `<p>${key}: ${value}</p>`;
     return html;
+  }
+
+  addToHTML(allValues) {
+    console.log('adding to HTML...');
+    const listValues = document.getElementById('listValues');
+    listValues.innerHTML;
+    listValues.insertAdjacentHTML('beforeend', allValues);
   }
 }
 
