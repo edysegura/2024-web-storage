b2762ea052bd0603d1379043f56b8bfe6acca4d5
Edy Segura <edysegura@gmail.com>
2024-07-13 11:11:45 -0300

    feat: multiple requests in parallel

--- cache-api/app.js
+++ cache-api/app.js
@@ -1,14 +1,20 @@
 const button = document.getElementById('fetchData');
 
 button.addEventListener('click', () => {
-  console.count('👀Button clicked!');
-  fetchData();
-  console.log('What? I was called first? 🤔');
+  console.count('👀 Button clicked!');
+  fetchCharacterData({ charId: 1 });
+  fetchCharacterData({ charId: 2 });
+  fetchCharacterData({ charId: 3 });
+  fetchCharacterData({ charId: 4 });
+  fetchCharacterData({ charId: 5 });
+  fetchCharacterData({ charId: 6 });
+  console.log('✅ Finished');
 });
 
-async function fetchData() {
-  const endpoint = 'https://swapi.dev/api/people/1';
+async function fetchCharacterData({ charId }) {
+  const endpoint = `https://swapi.dev/api/people/${charId}`;
+  console.log(`[fetchCharacterData] #${charId}`);
   const response = await fetch(endpoint);
   const character = await response.json();
-  console.log(character);
+  console.log(`🤺 Character name (${charId}): ${character.name}`);
 }
