5a7a172c8b1d0f7d30e089a5a5a8f1f839a96b49
Edy Segura <edysegura@gmail.com>
2024-07-13 16:14:33 -0300

    feat: map to html

--- localStorage/app.js
+++ localStorage/app.js
@@ -24,8 +24,16 @@ class App {
   listValues() {
     console.log('listing data...');
     const ls = window.localStorage;
+    if (!ls.length) return;
     const lsKeys = Object.keys(ls);
-    console.log(lsKeys);
+    const allValues = lsKeys.map(this.toHTML);
+    console.log(allValues);
+  }
+
+  toHTML(key) {
+    const value = window.localStorage.getItem(key);
+    const html = `<p>${key}: ${value}</p>`;
+    return html;
   }
 }
 
