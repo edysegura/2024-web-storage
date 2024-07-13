b414e5c5cff1a10fbd8984f4229d661ecdb8bfa5
Edy Segura <edysegura@gmail.com>
2024-07-13 10:50:24 -0300

    feat: requesting the endpoint

--- cache-api/app.js
+++ cache-api/app.js
@@ -1,5 +1,14 @@
-const button = document.getElementById("fetchData");
+const button = document.getElementById('fetchData');
 
-button.addEventListener("click", () => {
-  console.count("👀Button clicked!");
+button.addEventListener('click', () => {
+  console.count('👀Button clicked!');
+  fetchData();
+  console.log('What? I was called first? 🤔');
 });
+
+async function fetchData() {
+  const endpoint = 'https://swapi.dev/api/people/1';
+  const response = await fetch(endpoint);
+  const character = await response.json();
+  console.log(character);
+}
