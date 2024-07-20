744f6aa1a58592584689f66ab299cfc07c66b9f9
Edy Segura <edysegura@gmail.com>
2024-07-20 13:49:44 -0300

    chore: testing method implementation

--- indexedDB/cep/app.js
+++ indexedDB/cep/app.js
@@ -3,15 +3,11 @@ import Dexie from 'https://cdn.jsdelivr.net/npm/dexie@4.0.8/+esm';
 async function extractCEPsOnly() {
   const response = await fetch('./CEPs.txt');
   const textData = await response.text();
-  const [firstLine] = textData.split('\n');
-  const [, , , cep] = firstLine.split(';');
-  console.log(firstLine);
-  console.log(cep);
+  const lines = textData.split('\n');
+  const extractCEP = (line) => line.split(';')[3];
+  const cepList = lines.map(extractCEP);
+  return cepList;
 }
-
-console.time('Extracting CEP');
-extractCEPsOnly();
-console.timeEnd('Extracting CEP');
 
 async function fetchCEPData(cep) {
   const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
@@ -19,6 +15,11 @@ async function fetchCEPData(cep) {
   console.log('response data: ', data);
   return data;
 }
+
+const cepList = await extractCEPsOnly();
+console.log(cepList[0]);
+const cepData = await fetchCEPData(cepList[0]);
+console.log(cepData);
 
 // const db = new Dexie('zipCodeDatabase');
 
