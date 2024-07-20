05fa3de22f1064d3b91f109921845a18dec56c49
Edy Segura <edysegura@gmail.com>
2024-07-20 16:36:22 -0300

    feat: lazy load

--- indexedDB/cep/app.js
+++ indexedDB/cep/app.js
@@ -1,8 +1,10 @@
-import { installData } from './install-data.js';
+// import Dexie from 'https://cdn.jsdelivr.net/npm/dexie@4.0.8/+esm';
 
 const linkToInstall = document.querySelector("a[href='#']");
 linkToInstall.addEventListener('click', async () => {
   console.log('install data...');
+  const { installData } = await import('./install-data.js');
+  console.log(installData);
   alert('Will install');
   // button.disabled = true;
   // button.setAttribute('aria-busy', true);
