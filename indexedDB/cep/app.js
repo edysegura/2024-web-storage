ce8727ea14856514bfdca88d5d32d060049bcac3
Edy Segura <edysegura@gmail.com>
2024-07-20 16:28:33 -0300

    feat: add form validation

--- indexedDB/cep/app.js
+++ indexedDB/cep/app.js
@@ -1,10 +1,16 @@
 import { installData } from './install-data.js';
 
-const button = document.querySelector('button');
-button.addEventListener('click', async () => {
+const linkToInstall = document.querySelector("a[href='#']");
+linkToInstall.addEventListener('click', async () => {
   console.log('install data...');
-  button.disabled = true;
-  button.setAttribute('aria-busy', true);
-  await installData();
-  button.removeAttribute('aria-busy');
+  alert('Will install');
+  // button.disabled = true;
+  // button.setAttribute('aria-busy', true);
+  // await installData();
+  // button.removeAttribute('aria-busy');
+});
+
+const form = document.querySelector('form');
+form.addEventListener('submit', () => {
+  alert('IM here!');
 });
