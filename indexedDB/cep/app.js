6af5a203982427eb9bd06794dda02a6888e713d6
Edy Segura <edysegura@gmail.com>
2024-07-20 13:41:07 -0300

    feat: load CSV to extract CEPs

--- indexedDB/cep/app.js
+++ indexedDB/cep/app.js
@@ -1,33 +1,50 @@
 import Dexie from 'https://cdn.jsdelivr.net/npm/dexie@4.0.8/+esm';
 
-const response = await fetch(`https://viacep.com.br/ws/37537144/json/`);
-const data = await response.json();
-console.log('response data: ', data);
+async function extractCEPsOnly() {
+  const response = await fetch('./CEPs.txt');
+  const textData = await response.text();
+  const [firstLine] = textData.split('\n');
+  const [, , , cep] = firstLine.split(';');
+  console.log(firstLine);
+  console.log(cep);
+}
 
-const db = new Dexie('zipCodeDatabase');
+console.time('Extracting CEP');
+extractCEPsOnly();
+console.timeEnd('Extracting CEP');
 
-db.version(1).stores({
-  zipCode: '++id,cep,localidade',
-});
+async function fetchCEPData(cep) {
+  const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+  const data = await response.json();
+  console.log('response data: ', data);
+  return data;
+}
 
-// {
-//   "cep": "37537-144",
-//   "logradouro": "Rua Três",
-//   "complemento": "",
-//   "unidade": "",
-//   "bairro": "Viana",
-//   "localidade": "Santa Rita do Sapucaí",
-//   "uf": "MG",
-//   "ibge": "3159605",
-//   "gia": "",
-//   "ddd": "35",
-//   "siafi": "5191"
-// }
+// const db = new Dexie('zipCodeDatabase');
 
-db.zipCode.add({
-  zipCode: data.cep.replace('-', ''),
-  state: data.uf,
-  location: data.localidade,
-  neighborhood: data.bairro,
-  phoneCode: data.ddd,
-});
+// db.version(1).stores({
+//   zipCode: '++id,cep,localidade',
+// });
+
+// // {
+// //   "cep": "37537-144",
+// //   "logradouro": "Rua Três",
+// //   "complemento": "",
+// //   "unidade": "",
+// //   "bairro": "Viana",
+// //   "localidade": "Santa Rita do Sapucaí",
+// //   "uf": "MG",
+// //   "ibge": "3159605",
+// //   "gia": "",
+// //   "ddd": "35",
+// //   "siafi": "5191"
+// // }
+
+// db.zipCode.add({
+//   zipCode: data.cep.replace('-', ''),
+//   state: data.uf,
+//   location: data.localidade,
+//   publicPlace: data.logradouro,
+//   neighborhood: data.bairro,
+//   phoneCode: data.ddd,
+// });
