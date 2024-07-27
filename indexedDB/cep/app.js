86197ee955a91a151bad5c111c64fae7721c846b
Edy Segura <edysegura@gmail.com>
2024-07-27 10:41:09 -0300

    feat: implement ux enhancement

--- indexedDB/cep/app.js
+++ indexedDB/cep/app.js
@@ -1,17 +1,5 @@
 import getZipCodeDatabase from './database.js';
 
-const linkToInstall = document.querySelector("a[href='#']");
-linkToInstall.addEventListener('click', async () => {
-  console.log('install data...');
-  const { installData } = await import('./install-data.js');
-  console.log(installData);
-  alert('Will install');
-  // button.disabled = true;
-  // button.setAttribute('aria-busy', true);
-  // await installData();
-  // button.removeAttribute('aria-busy');
-});
-
 async function getCepData(zipCode) {
   const db = await getZipCodeDatabase();
   let zipCodeData = await db.zipCode.get(zipCode);
@@ -33,12 +21,30 @@ function fillTable(zipCodeData) {
   Object.keys(zipCodeData).forEach(addToTheTable);
 }
 
+const linkToInstall = document.querySelector("a[href='#']");
+linkToInstall.addEventListener('click', async () => {
+  console.log('install data...');
+  const { installData } = await import('./install-data.js');
+  console.log(installData);
+  alert('Will install');
+  // button.disabled = true;
+  // button.setAttribute('aria-busy', true);
+  // await installData();
+  // button.removeAttribute('aria-busy');
+});
+
 const form = document.querySelector('form');
+const submitButton = document.querySelector("button[type='submit']");
 form.addEventListener('submit', async () => {
+  // set load true
+  submitButton.setAttribute('aria-busy', true);
+  submitButton.disabled = true;
   const zipCodeData = await getCepData(form.cep.value.replace('-', ''));
+  // set load off
+  submitButton.setAttribute('aria-busy', false);
+  submitButton.disabled = false;
   fillTable(zipCodeData);
 });
 
 // TODO: improve this code
-// TODO: if not find in the indexedDB fetch from the network and save into the table
 // TODO: format CEP data to show in the table
